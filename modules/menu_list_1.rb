274e4ff72d1a099eb43a0d5eab571be9cf1545b6
Natasha Madondo <tasha.madondo@gmail.com>
2021-09-26 17:20:14 +1000

    Menu list 2b added and linked to menu list 1

--- modules/menu_list_1.rb
+++ modules/menu_list_1.rb
@@ -1,16 +1,19 @@
 require 'tty-prompt'
 require_relative './menu_list_2_a'
+require_relative './menu_list_2_b'
 
 def menu_list_1
     choices = { Food: 1, Attractions: 2 }
     prompt1 = TTY::Prompt.new
     puts 'Welcome to the interactive travel guide!'
-    prompt1.select('What is more important to you when travelling?', choices)
+    prompt1.select('What is more important to you when travelling? (Use ↑/↓ and ←/→ arrow keys, press Enter to select)', choices)
    
 end
 
 if menu_list_1 == 1 
+    system("clear")
     menu_list_2a
 else
-    puts "bye"
+    system("clear")
+    menu_list_2b
 end
---
+++ modules/menu_list_2_b.rb
@@ -0,0 +1,13 @@
+require 'tty-prompt'
+
+def menu_list_2b
+    menu = {
+            'Snow festival' => 1,
+            'Imperial Palace' => 2,
+            'Kiyomizu-dera' => 3,
+            'Beppu Onsen' => 4
+        }
+    prompt = TTY::Prompt.new
+    prompt.select("Pick an activity you'd like to try!", menu, symbols: { marker: '❀' })
+end
+
