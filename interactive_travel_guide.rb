df6cf74cd0a171f49ebcd3aefd940cf6cef36577
Natasha Madondo <tasha.madondo@gmail.com>
2021-09-28 17:48:44 +1000

    Added ascii art for quiz, added japanese to quiz questions, added ascii art to premable, put preamble method in main app file

--- interactive_travel_guide.rb
+++ interactive_travel_guide.rb
@@ -2,6 +2,7 @@ require_relative './modules/welcome'
 require_relative './modules/help_selection'
 require_relative './modules/link_help'
 require_relative './modules/menu_list_1'
+require_relative './modules/prequiz'
 require_relative './modules/get_quiz'
 
 # Welcome message and get user name
@@ -18,6 +19,7 @@ if help_selection == true
 end
 
 # Menu lists 1 and 2 link
+system('clear')
 if menu_list_1 == 1
     system('clear')
     menu_list_2a
@@ -27,7 +29,11 @@ else
 end
 
 # Preamble to Quiz 
-
+preamble
 
 # Quiz section
 get_quiz
+
+# User nswer to question
+puts "Please input the number of your answer:"
+answer = gets.chomp.to_i
--- modules/prequiz.rb
+++ modules/prequiz.rb
@@ -1,6 +1,16 @@
 def preamble
     system('clear')
     puts '止まれ(tomare!)'
+    sleep(0.5)
+    puts 'Stop!'
+    sleep(0.5)
+    File.foreach ('./text_files/stop_ascii.txt') do |line|
+        puts line
+    end
+    sleep(0.5)
     puts 'You have just activated my trap card!'
     puts 'To continue you must answer the following question'
+    puts ' '
+    puts ' '
 end
+
